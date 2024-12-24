fbd4416cc8951913038f09a3360ab828d52bb9b3
spectresridharan <shridharan67890@gmail.com>
2024-12-24 09:17:40 +0530

    Update script.js

--- agroroots/script.js
+++ agroroots/script.js
@@ -2,4 +2,21 @@ var swiper=new Swiper(".home",{
   spaceBetween: 30;
 centeredSlides: true,
 navigation:{
-nextE1:".swiper
+  nextE1:".swiper-button-next",
+  prevE1:".swiper-button-prev",
+    },
+});
+let men=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menu.onclick=()=>{
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+window.onscroll=()=>{
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+}
+
+  
+
